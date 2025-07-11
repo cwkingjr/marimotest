52586ecbd4eac8df22e4cf8455b5919d20b2fa02
Chuck King <cwkingjr@gmail.com>
2025-07-10 20:18:11 -0500

    added sidebar and other views

--- mytest.py
+++ mytest.py
@@ -8,7 +8,31 @@ app = marimo.App(width="full", app_title="SIM Ticket Explorer")
 def _():
     import marimo as mo
     import pandas as pd
-    df = pd.read_csv("fake-data-3000-rows.csv")
+    return mo, pd
+
+
+@app.cell
+def _(mo, pd):
+    # load the data file
+    params = mo.cli_args()
+    infile = params["infile"]
+    df = pd.read_csv(infile)
+    return df, infile
+
+
+@app.cell
+def _():
+    # TODO confirm that the infile has the correct schema or notify user of what is expected and quit processing
+
+    # TODO determine if we will accept either csv or xlsx and if they will have the same schemas
+    return
+
+
+@app.cell
+def _(df, pd):
+    # clean up the data before we use it
+
+    # rename the columns to make them easier to see in tables and graphs
     df.rename(
         columns={
             "tpa_from (string)": "tpa_from",
@@ -22,9 +46,82 @@ def _():
         },
         inplace=True,
     )
+
+    # change the date columns from strings to datetimes so we can use the datetime methods on them
     df["Created"] = pd.to_datetime(df.Created, format="%m/%d/%y %H:%M")
     df["LastUpdated"] = pd.to_datetime(df.LastUpdated, format="%m/%d/%y %H:%M")
+    return
+
+
+@app.cell
+def _(infile, mo):
+    mo.sidebar(
+        [
+            mo.md("##Explorer Info"),
+            mo.md(f"infile:{infile}")
+        ]
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md("""#Dataframe View of Cleaned Up Data""")
+    return
+
+
+@app.cell
+def _(df):
+    # show the dataframe in a rich table
     df
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""#Data By Assignee""")
+    return
+
+
+@app.cell
+def _(df, mo):
+    assignee_select = mo.ui.dropdown.from_series(
+        df["Assignee"], label="Assignee"
+    )
+    mo.hstack([mo.md("Filter Dataframe By:"),assignee_select,mo.md("Note: You can actually filter any/all columns in the dataframe view above by clicking on the column header and choosing filter. This is just here to make it quicker.")],justify="start")
+    return (assignee_select,)
+
+
+@app.cell
+def _(assignee_select, df, mo):
+    filtered_df = df[df["Assignee"] == assignee_select.value]
+    mo.ui.table(filtered_df)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""#Transform View of Datafame""")
+    return
+
+
+@app.cell
+def _(df, mo):
+    transformed_df = mo.ui.dataframe(df)
+    transformed_df
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""#Explorer View of Datafame""")
+    return
+
+
+@app.cell
+def _(df, mo):
+    explored_df = mo.ui.data_explorer(df)
+    explored_df
     return
 
 
